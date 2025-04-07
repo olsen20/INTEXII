8714dc24221ab1964fb62e26172f5d2cbdc5fc82
lonni2020 <153124510+lonni2020@users.noreply.github.com>
2025-04-07 12:14:46 -0600

    Redeploying backend

--- backend/Intex.API/Program.cs
+++ backend/Intex.API/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
     policy =>
     {
-        policy.WithOrigins("http://localhost:3000")
+        policy.WithOrigins("http://localhost:3000", "https://yellow-grass-01700f61e.6.azurestaticapps.net/")
             .AllowAnyMethod()
             .AllowAnyHeader();
     }));
