0f2ef371abea3026a99f1e738efb88ed5db674e7
olsen20 <olsen20@byu.edu>
2025-04-07 14:22:01 -0600

    Created Movie Controllers

--- backend/Intex.API/Data/MovieRating.cs
+++ backend/Intex.API/Data/MovieRating.cs
@@ -1,10 +1,18 @@
-﻿namespace Intex.API.Data
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Intex.API.Data
 {
+    [Table("movie_ratings")]
     public class MovieRating
     {
+        [Key, Column("user_id", Order = 0)]
         public string UserId { get; set; }
+
+        [Key, Column("show_id", Order = 1)]
         public string ShowId { get; set; }
+
+        [Column("rating")]
         public decimal Rating { get; set; }
     }
-
 }
