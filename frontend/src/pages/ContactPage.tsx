e8550fe99c7bc1f4aab3b8abde4d1ae129527f4e
bmredden <bentleyredden@gmail.com>
2025-04-07 21:51:03 -0600

    added hero banner and fixed formatting

--- frontend/src/pages/ContactPage.tsx
+++ frontend/src/pages/ContactPage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import TableOfContents from "../components/TableofContents";
+import TableOfContents from "../components/TableOfContents";
 
 const ContactPage: React.FC = () => {
   return (
