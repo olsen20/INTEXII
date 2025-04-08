9ec4199e3dc7843267478b9d644ca9e2b3697fe7
lonni2020 <153124510+lonni2020@users.noreply.github.com>
2025-04-08 16:02:39 -0600

    Added Controllers and data for recommendations

---
+++ backend/Intex.API/Data/CollaborativeModel.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Intex.API.Data
+{
+    [Table("collaborative_recommendations")]
+    public class CollaborativeModel
+    {
+
+        [Key]
+        [Column("if_you_liked")]
+        public string ShowId { get; set; }
+
+        [Column("recommendation_1")]
+        public string ReccomendationOne { get; set; }
+
+        [Column("recommendation_2")]
+        public string ReccomendationTwo { get; set; }
+
+        [Column("recommendation_3")]
+        public string ReccomendationThree { get; set; }
+
+        [Column("recommendation_4")]
+
+        public string ReccomendationFour { get; set; }
+
+        [Column("recommendation_5")]
+        public string ReccomendationFive { get; set; }
+
+        [Column("recommendation_6")]
+        public string ReccomendationSix { get; set; }
+
+        [Column("recommendation_7")]
+        public string ReccomendationSeven { get; set; }
+
+        [Column("recommendation_8")]
+        public string ReccomendationEight { get; set; }
+
+        [Column("recommendation_9")]
+        public string ReccomendationNine { get; set; }
+
+        [Column("recommendation_10")]
+        public string ReccomendationTen { get; set; }
+    }
+
+}
+
---
+++ backend/Intex.API/Data/UserBaseModel.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Intex.API.Data
+{
+    [Table("hybrid_recommender")]
+    public class UserBasedModel
+    {
+        [Key]
+        [Column("user_id")]
+        public int UserId { get; set; }
+
+        [Column("recommendation_1")]
+        public string ReccomendationOne { get; set; }
+
+        [Column("recommendation_2")]
+        public string ReccomendationTwo { get; set; }
+
+        [Column("recommendation_3")]
+        public string ReccomendationThree { get; set; }
+
+        [Column("recommendation_4")]
+
+        public string ReccomendationFour { get; set; }
+
+        [Column("recommendation_5")]
+        public string ReccomendationFive { get; set; }
+
+        [Column("recommendation_6")]
+        public string ReccomendationSix { get; set; }
+
+        [Column("recommendation_7")]
+        public string ReccomendationSeven { get; set; }
+
+        [Column("recommendation_8")]
+        public string ReccomendationEight { get; set; }
+
+        [Column("recommendation_9")]
+        public string ReccomendationNine { get; set; }
+
+        [Column("recommendation_10")]
+        public string ReccomendationTen { get; set; }
+    }
+}
