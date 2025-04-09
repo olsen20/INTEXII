b9303cd75a1955fb49e27930de82c04a2f84407c
bmredden <bentleyredden@gmail.com>
2025-04-09 17:05:22 -0600

    admin button formatting

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -62,7 +62,7 @@ const Header: React.FC = () => {
               {/* Conditional admin button */}
               {!isLoading && role === "Administrator" && (
                 <li className="nav-item me-5">
-                  <Link className="nav-link" to="/admin">
+                  <Link className="button-fade-in nav-link" to="/admin">
                     Admin
                   </Link>
                 </li>
